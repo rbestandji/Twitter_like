3c3cb2fc27728ed42b5e65bac93700126143b86e
Pasquet <le.jitou@gmail.com>
2012-10-28 01:55:53 +0200

    Tests d'integration pour la creation de compte et la connexion

---
+++ src/test/java/tests/it/WebappIT.java
@@ -0,0 +1,217 @@
+package tests.it;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.api.representation.Form;
+import com.sun.jersey.client.apache.ApacheHttpClient;
+import com.sun.jersey.client.apache.config.ApacheHttpClientConfig;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import junit.framework.TestCase;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.junit.Test;
+import com.sun.jersey.client.apache.config.DefaultApacheHttpClientConfig;
+import core.Status;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.util.Date;
+import model.Message;
+import model.User;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+
+public class WebappIT extends TestCase {
+
+  private String baseUrl;
+  private DefaultApacheHttpClientConfig config;
+  private ApacheHttpClient client;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    String port = System.getProperty("servlet.port");
+    this.baseUrl = "http://localhost:" + port + "/cargo-webapp";
+
+    config = new DefaultApacheHttpClientConfig();
+    
+    //Autorisation des cookies
+    config.getProperties().put(ApacheHttpClientConfig.PROPERTY_HANDLE_COOKIES, true);
+
+    //Jersey configuration
+    config.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+
+    //Client apache :)
+    client = ApacheHttpClient.create(config);
+    client.setFollowRedirects(Boolean.TRUE);
+
+  }
+
+  /*@Test
+   public void lancementTests() throws Exception {
+   System.out.println("Lancement des tests");
+   testCreationDesUsers();
+   testConnectionCompte();
+   }*/
+
+  /* Dans cette fonction nous allons creer des utilisateurs */
+  @Before
+  public void testCreationDesUsers() throws Exception {
+
+    Form f = new Form();
+    WebResource webResource = null;
+    ClientResponse result = null;
+    System.out.println("Creation des comptes ! ");
+
+    /* Utilisateur 1 */
+    f.add("email", "le.jitou@gmail.com");
+    f.add("mdp", "mdp");
+    f.add("nom", "Pasquet");
+    f.add("prenom", "Jerome");
+    webResource = client.resource(new URL(this.baseUrl + "/inscription").toURI());
+    result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+    Assert.assertEquals(result.getStatus(), Status.OK);
+
+
+    /* Utilisateur 2 */
+    f.clear();
+    f.add("email", "lavalber02@gmail.com");
+    f.add("mdp", "monMDP");
+    f.add("nom", "Laval");
+    f.add("prenom", "Bernard");
+    webResource = client.resource(new URL(this.baseUrl + "/inscription").toURI());
+    result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+    Assert.assertEquals(result.getStatus(), Status.OK);
+
+
+    /* Utilisateur 3 */
+    /*f.clear();
+    f.add("email", "lionel.muller.34@gmail.com");
+    f.add("mdp", "monMDP");
+    f.add("nom", "Muller");
+    f.add("prenom", "Lionel");
+    webResource = client.resource(new URL(this.baseUrl + "/inscription").toURI());
+    result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+    Assert.assertEquals(result.getStatus(), Status.OK);
+    * */
+
+  }
+
+  /* Cette fonction test qu'un utilisateur peut se connecter avec (qu'avec) son compte.*/
+  @After
+  public void testConnectionCompte() throws Exception {
+    Form f = new Form();
+    WebResource webResource;
+    ClientResponse result;
+    System.out.println("Tests des connexions! ");
+
+
+    /* Connexion de l'utilisateur 1 */
+    f.add("email", "le.jitou@gmail.com");
+    f.add("mdp", "mdp");
+    webResource = client.resource(new URL(this.baseUrl + "/connection").toURI());
+    result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+    Assert.assertEquals(result.getStatus(), Status.OK);
+
+    
+    /* L'utilisateur 1 est encore connecté tentative de connexion de l'utilisateur 2 */
+    f.clear();
+    f.add("email", "lavalber02@gmail.com");
+    f.add("mdp", "monMDP");
+    webResource = client.resource(new URL(this.baseUrl + "/connection").toURI());
+    result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+    Assert.assertEquals(result.getStatus(), Status.UTILISATEUR_CONNECTE);
+
+
+    /* L'utilisateur 1 se deconnecte */
+    webResource = client.resource(new URL(this.baseUrl + "/bye").toURI());
+    result = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+    System.out.println(result.getEntity(String.class));
+
+
+    /* L'utilisateur 2  veut se reconnecter avec le mauvais mdp */
+    f.clear();
+    f.add("email", "lavalber02@gmail.com");
+    f.add("mdp", "mdp");
+    webResource = client.resource(new URL(this.baseUrl + "/connection").toURI());
+    result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+    Assert.assertEquals(result.getStatus(), Status.UTILISATEUR_MAUVAIS_MOT_PASS);
+
+    /* L'utilisateur 1  veut se reconnecter avec le mauvais identifiant */
+    f.clear();
+    f.add("email", "le.jidtouu@gmail.com");
+    f.add("mdp", "mdp");
+    webResource = client.resource(new URL(this.baseUrl + "/connection").toURI());
+    result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+    Assert.assertEquals(result.getStatus(), Status.UTILISATEUR_PAS_DE_COMPTE);
+
+  }
+}
+
+
+
+    /*
+    
+     Form f = new Form();
+  
+     WebResource webResource = null;
+     ClientResponse result = null;
+    
+     webResource = c.resource(new URL(this.baseUrl + "/inscription").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+     System.out.println(result.getEntity(String.class));
+    
+    
+     webResource = c.resource(new URL(this.baseUrl + "/connection").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+     System.out.println(result.getEntity(String.class));
+    
+    
+     webResource = c.resource(new URL(this.baseUrl + "/bye").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+     System.out.println(result.getEntity(String.class));
+    
+     f.clear();
+     f.add("email", "email");
+     f.add("mdp", "mdp");
+     webResource = c.resource(new URL(this.baseUrl + "/connection").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+     System.out.println(result.getStatus());
+     System.out.println(result.getEntity(String.class));
+    
+     webResource = c.resource(new URL(this.baseUrl + "/messages/my").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+     System.out.println("List messages : " + result.getEntity(List.class));
+    
+    
+    
+     f.clear();
+     f.add("msg", "Hello tout le monde");
+     webResource = c.resource(new URL(this.baseUrl + "/messages/envoie").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, f);
+     System.out.println(result.getEntity(String.class));
+    
+     webResource = c.resource(new URL(this.baseUrl + "/messages/my").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+     System.out.println("List messages : " + result.getEntity(List.class));
+    
+     webResource = c.resource(new URL(this.baseUrl + "/messages/get/1").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+     System.out.println("List messages : " + result.getEntity(List.class));
+    
+    
+     webResource = c.resource(new URL(this.baseUrl + "/users/get/1").toURI());
+     result = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+     System.out.println("User 1 : " + result.getEntity(String.class));
+     */
